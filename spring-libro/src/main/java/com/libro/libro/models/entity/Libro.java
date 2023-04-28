bd83772338337c5a5b30a817830720c584478629
Ulises Gadea <ulisesgadea28@gmail.com>
2023-04-27 21:50:51 -0300

    agregando pa probar

---
+++ spring-libro/src/main/java/com/libro/libro/models/entity/Libro.java
@@ -0,0 +1,7 @@
+package com.libro.libro.models.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Libro {
+}
---
+++ spring-libro/src/main/java/com/libro/libro/models/service/IntLibroService.java
@@ -0,0 +1,4 @@
+package com.libro.libro.models.service;
+
+public interface IntLibroService {
+}
