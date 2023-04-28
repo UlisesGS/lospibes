8a8895afdd6a53b27ebc30e019efde668d8d5473
Ulises Gadea <ulisesgadea28@gmail.com>
2023-04-27 21:18:58 -0300

    iniciando el project

---
+++ spring-autor/src/test/java/com/autor/demo/AutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-editorial/src/test/java/com/editorial/editorial/EditorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.editorial.editorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
